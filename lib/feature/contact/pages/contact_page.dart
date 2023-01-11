c42f1e52cee50346ce96f3f4e64b9f1ac17614a2
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2023-01-11 21:58:25 +0700

    Menambahkan contact page (contact_page.dart)

---
+++ lib/feature/contact/pages/contact_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/common/widgets/custom_icon_button.dart';
+
+class ContactPage extends StatelessWidget {
+  const ContactPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const BackButton(),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              "Select contact",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 3,
+            ),
+            Text(
+              '5 contact',
+              style: TextStyle(fontSize: 12),
+            ),
+          ],
+        ),
+        actions: [
+          CustomIconButton(onTap: () {}, icon: Icons.search),
+          CustomIconButton(onTap: () {}, icon: Icons.more_vert),
+        ],
+      ),
+    );
+  }
+}
